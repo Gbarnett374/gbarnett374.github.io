3a80c18b0f480a467074da64ac5f20cfd5b56d02
Greg Barnett <Gbarnett374@gmail.com>
2015-11-01 16:03:58 -0500

    fixed tabs

--- user_tracking.php
+++ user_tracking.php
@@ -7,8 +7,8 @@ require './include/db.php';
  */
 function logUser($dbc)
 {
-	$ip_address = $dbc->real_escape_string($_SERVER['REMOTE_ADDR']);
-	$user_agent = $dbc->real_escape_string($_SERVER['HTTP_USER_AGENT']);
+    $ip_address = $dbc->real_escape_string($_SERVER['REMOTE_ADDR']);
+    $user_agent = $dbc->real_escape_string($_SERVER['HTTP_USER_AGENT']);
 
     $sql = "INSERT INTO user_tracking
     (ip_address, user_agent)
