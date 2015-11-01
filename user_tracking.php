d678a2e5610655696c024690541c1994f9c9f3c4
Greg Barnett <Gbarnett374@gmail.com>
2015-11-01 15:55:44 -0500

    fixes to sql

--- user_tracking.php
+++ user_tracking.php
@@ -7,17 +7,17 @@ require './include/db.php';
  */
 function logUser($dbc)
 {
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-	$user_agent = $_SERVER['HTTP_USER_AGENT'];
+	$ip_address = $dbc->real_escape_string($_SERVER['REMOTE_ADDR']);
+	$user_agent = $dbc->real_escape_string($_SERVER['HTTP_USER_AGENT']);
 
-	$sql = "INSERT INTO user_tracking
-	(ip_address, user_agent)
-	VALUES({$ip_address},{$user_agent})";
+    $sql = "INSERT INTO user_tracking
+    (ip_address, user_agent)
+    VALUES('{$ip_address}','{$user_agent}')";
 
-	$dbc->query($sql);
+    $dbc->query($sql);
 }
 
-loguser($dbc);
+logUser($dbc);
 
 //Close Connection. 
 $dbc->close();
