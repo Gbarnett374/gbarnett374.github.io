0c3125d1d0763ad049d0dd02c23083486c72a265
gbarnett374 <gbarnett374@gmail.com>
2016-02-08 20:22:58 -0500

    Code cleanup

--- user_tracking.php
+++ user_tracking.php
@@ -3,16 +3,16 @@ require './include/db.php';
 
 /**
  * Grabs the user info from $_SERVER and inserts it into a table. 
- * @param $dbc - The db connection object. 
+ * @param $dbc - The db connection object.
  */
 function logUser($dbc)
 {
-    $ip_address = $dbc->real_escape_string($_SERVER['REMOTE_ADDR']);
-    $user_agent = $dbc->real_escape_string($_SERVER['HTTP_USER_AGENT']);
+    $ipAddress = $dbc->real_escape_string($_SERVER['REMOTE_ADDR']);
+    $userAgent = $dbc->real_escape_string($_SERVER['HTTP_USER_AGENT']);
 
     $sql = "INSERT INTO user_tracking
     (ip_address, user_agent)
-    VALUES('{$ip_address}','{$user_agent}')";
+    VALUES('{$ipAddress}','{$userAgent}')";
 
     $dbc->query($sql);
 }
